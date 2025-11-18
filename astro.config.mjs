30c97876928e14c4165a6cfda65026946312559a
mpoapostolis <mpoapostolis@gmail.com>
2025-11-18 21:37:35 +0200

    seo-shit
    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mposoft.com', // Replace with actual domain if different, but required for sitemap
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [sitemap()]
 });
